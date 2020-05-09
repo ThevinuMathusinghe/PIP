1493b33eb618b68325137b90bddbe4e30f3bf33b
uskeenonmedia <officialkcwebx@gmail.com>
2020-05-09 15:56:22 +1200

    Added the models for the database folder

---
+++ server/models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+const databaseUrl = 'mongodb://localhost:27017/pip';
+mongoose
+  .connect(databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports.User = require('./user');
