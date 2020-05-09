2b4128c74c65507e29aa9ffabb4a7582ddf46d4d
uskeenonmedia <officialkcwebx@gmail.com>
2020-05-09 16:23:29 +1200

    Added the hashing to the user

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -14,5 +15,27 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    let hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candiatePassword, next) {
+  try {
+    let isMatch = await bcrypt.compare(candiatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = mongoose.model('User', userSchema);
