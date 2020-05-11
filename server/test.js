b58bb32c31dbeff2799361851c53482023f157bd
tdm23 <thevinu777@gmail.com>
2020-05-11 20:16:18 +1200

    Started pulling data from websites it self

--- server/test.js
+++ server/test.js
@@ -1,10 +1,52 @@
-var links = document.querySelectorAll('.column-2');
-var linksArray = Array.from(links);
+var title = document.querySelectorAll('h1')
+console.log (title);
 
-linksArray = linksArray.filter((link, i) => {
-  return i != 0;
-});
+var description = document.querySelectorAll('div.ci-content-fullwidth p')
+var descriptionArray = Array.from(description);
+var descriptionString= descriptionArray[1].innerText;
+console.log (descriptionString);
 
-linksArray = linksArray.map((link) => {
-  return link.childNodes[0].href;
-});
+
+var revenue = document.querySelectorAll('td')
+var revenueArray = Array.from(revenue);
+var revenueData = "";
+for (var i = 0; i < revenueArray.length; i++){
+    if ( revenueArray[i].innerText == "Revenue"){
+        v = revenueArray[i+1].innerText;
+    }
+}
+console.log (revenueData);
+
+
+var phoneNumber = document.querySelectorAll('td')
+var phoneNumberArray = Array.from(phoneNumber);
+var phoneNumberData = "";
+for (var i = 0; i < phoneNumberArray.length; i++){
+    if ( phoneNumberArray[i].innerText == "Phone" || phoneNumberArray[i].innerText == "Freecall"){
+        phoneNumberData = phoneNumberArray[i+1].innerText;
+    }
+}
+console.log (phoneNumberData);
+
+
+var website = document.querySelectorAll('td')
+var websiteArray = Array.from(website);
+var websiteData = "";
+for (var i = 0; i < websiteArray.length; i++){
+    if (websiteArray[i].innerText == "Website"){
+        websiteData = websiteArray[i+1].innerText;
+    }
+}
+console.log (websiteData);
+
+//INFORMATION
+
+var informationTitle = document.querySelectorAll('div.boxCompanyInfo td a')
+var informationTitleArray = Array.from(informationTitle);
+var informationTitleDataArray = [];
+for (var i = 0; i < informationTitleArray.length; i+=3){
+    informationTitleDataArray.push(informationTitleArray[i].innerText)
+}
+
+console.log (informationTitleDataArray);
+
