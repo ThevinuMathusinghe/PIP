69743916fb7265bd7477f0224468193a9d5fa900
tdm23 <thevinu777@gmail.com>
2020-05-11 20:16:18 +1200

    Started pulling data from websites it self

--- server/helpers/crawler.js
+++ server/helpers/crawler.js
@@ -90,3 +90,53 @@ exports.getCompanyLinks = async (req, res, next) => {
     res.json({ error: 'Something went wrong', status: 400 });
   }
 };
+
+exports.getCompanyData = async (req, res, next) => {
+  try{
+    console.log('started');
+    const rawLinks = fs.readFileSync (
+      path.join(__dirname, './links/companyLinks.txt'),
+      'utf-8'
+    );
+    let links = rawLinks.split(',');
+
+    const browser = await puppeteer.launch({headless: false});
+
+    let companyData = [];
+    let count = 0; 
+
+    const interval = setInterval(() => {
+      console.log(count);
+      if (count >= 5) {
+        //store in data base
+
+        clearInterval(interval);
+        browser.close();
+        next();
+      } else {
+        for ( let i = count * 5; i < count * 5 + 5; i++){
+          (async () => {
+            try {
+              const page = await browser.newPage();
+              await page.setViewport({ width:1980, height: 1080});
+              await page.goto(links[i]);
+
+              //Find the data in the links
+              
+            
+            }catch (err){
+              console.log(err);
+            }
+          })();
+        }
+      }
+
+
+
+
+    },5000);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
