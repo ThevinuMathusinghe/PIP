42d467888337b9cfec3e9dc3ee3ac3a7cca9d092
sd169 <sajith.dhambagolla@gmail.com>
2020-05-13 14:26:44 +1200

    Added print statment

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void newTest() async {
     try {
+      print("test");
       final File imageFile =
           await ImagePicker.pickImage(source: ImageSource.gallery);
       if (imageFile == null) return;
