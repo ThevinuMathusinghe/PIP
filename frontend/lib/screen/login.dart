b7780143c39c437a6bb1f4d49a0b1855b767edb6
uskeenonmedia <officialkcwebx@gmail.com>
2020-05-20 01:40:19 +1200

    fixed the login

--- frontend/lib/screen/login.dart
+++ frontend/lib/screen/login.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class Login extends StatefulWidget {
   @override
   _Login createState() => _Login();
-  
 }
-
 
 class _Login extends State<Login> {
   String email;
@@ -16,16 +13,14 @@ class _Login extends State<Login> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-Future<String> attemptLogIn(String email, String password) async{
-var res = await http.post("https://limitless-meadow-18984.herokuapp.com/user/login",
-body: {
-  "email": email,
-  "password": password
+  Future<String> attemptLogIn(String email, String password) async {
+    var res = await http.post(
+        "https://limitless-meadow-18984.herokuapp.com/user/login",
+        body: {"email": email, "password": password});
+    print(res.body);
+    if (res.statusCode == 200) return res.body;
+    return null;
   }
-);
-if(res.statusCode == 200) return res.body;
-return null;
-}
 
   @override
   Widget build(BuildContext context) {
@@ -33,14 +28,15 @@ return null;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
+          child: SingleChildScrollView(
+              child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Container(height: MediaQuery.of(context).padding.top),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
             child: TextField(
+              controller: _emailController,
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.person),
                 labelText: 'Email',
@@ -59,6 +55,7 @@ return null;
           Padding(
             padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
             child: TextField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.person),
                 labelText: 'Password',
@@ -74,24 +71,23 @@ return null;
               },
             ),
           ),
-          
           Padding(
             padding: EdgeInsets.symmetric(vertical: height * .1),
-            child: RaisedButton(             
+            child: RaisedButton(
               color: Colors.lightBlueAccent,
               onPressed: () async {
-                SharedPreferences myPrefs = await SharedPreferences.getInstance();               
+                SharedPreferences myPrefs =
+                    await SharedPreferences.getInstance();
                 email = _emailController.text;
                 password = _passwordController.text;
                 var jwt = await attemptLogIn(email, password);
-                if(jwt != null){
+                if (jwt != null) {
                   myPrefs.setString("jwt", jwt);
                   //Navigator.of(context).pushNamed("/login");
+                } else {
+                  return ("No account was found that matches that email and password");
                 }
-                else{
-                  return("No account was found that matches that email and password");
-                }                
-              },  
+              },
               child: Text(
                 'Login',
                 style: TextStyle(color: Colors.white),
@@ -99,9 +95,7 @@ return null;
             ),
           )
         ],
-        )
-      )
-      ),
+      ))),
     );
   }
 }
