76dd9eaaba16d4b1a4a25d0c64552e455ad628e9
sd169 <sajith.dhambagolla@gmail.com>
2020-05-19 14:22:39 +1200

    installed shared preferences

--- frontend/lib/screen/login.dart
+++ frontend/lib/screen/login.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-//import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:io';
-import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 class Login extends StatefulWidget {
