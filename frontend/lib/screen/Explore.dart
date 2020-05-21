f2ce8978a95973db87f990b60fd016a11c6ac48b
sd169 <sajith.dhambagolla@gmail.com>
2020-05-22 01:42:43 +1200

    adding to explore page

--- frontend/lib/screen/Explore.dart
+++ frontend/lib/screen/Explore.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:camera/camera.dart';
 
 class Explore extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+
+    Future<void> _optionsDialogBox(){
+      return showDialog(context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                GestureDetector(
+                  child: new Text('Capture your logo'),
+                  //onTap: openCamera,
+                  )
+              ]
+            )
+          ))
+      }
+      )
+    }
     
     return MaterialApp(    
       home: new Container(
