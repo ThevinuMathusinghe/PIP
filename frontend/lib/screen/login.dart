cf5edd84150a323c1397863034a9827662149f46
uskeenonmedia <officialkcwebx@gmail.com>
2020-05-21 23:07:42 +1200

    login request done

--- frontend/lib/screen/login.dart
+++ frontend/lib/screen/login.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/error.dart';
+import 'package:frontend/widgets/loading.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'dart:convert';
 
 class Login extends StatefulWidget {
   @override
@@ -15,46 +18,77 @@ class _Login extends State<Login> {
   bool isLoggedIn = false;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool loading = false;
+  String errorMessage = "";
 
-  void onLoginStatusChanged(bool isLoggedIn){
-    setState((){
+  void onLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
       this.isLoggedIn = isLoggedIn;
-    }
-    );
+    });
   }
 
-  void initiateFacebookLogin() async{
+  void initiateFacebookLogin() async {
     var fbLogin = FacebookLogin();
     var fbLoginResult = await facebookLogin.logIn(['email']);
-    switch(fbLoginResult.status){
+    switch (fbLoginResult.status) {
       case FacebookLoginStatus.error:
-      print("Error logging in");
-      onLoginStatusChanged(false);
-      break;
+        print("Error logging in");
+        onLoginStatusChanged(false);
+        break;
       case FacebookLoginStatus.cancelledByUser:
-      print("Cancelled by User");
-      onLoginStatusChanged(false);
-      break;
+        print("Cancelled by User");
+        onLoginStatusChanged(false);
+        break;
       case FacebookLoginStatus.loggedIn:
-      print("Login Successfull");
-      onLoginStatusChanged(true);
-      break;
+        print("Login Successfull");
+        onLoginStatusChanged(true);
+        break;
     }
   }
 
-  Future<String> attemptLogIn(String email, String password) async {
-    var res = await http.post(
-        "https://limitless-meadow-18984.herokuapp.com/user/login",
-        body: {"email": email, "password": password});
-    print(res.body);
-    if (res.statusCode == 200) return res.body;
-    return null;
+  void login() async {
+    try {
+      setState(() {
+        loading = true;
+      });
+      var res = await http.post(
+          "https://limitless-meadow-18984.herokuapp.com/user/login",
+          body: {
+            "email": _emailController.text,
+            "password": _passwordController.text
+          });
+      var response = json.decode(res.body);
+
+      if (response['error'] != null) {
+        setState(() {
+          errorMessage = response['error']['message'];
+        });
+      }
+      if (response['token'] != null) {
+        SharedPreferences myPrefs = await SharedPreferences.getInstance();
+        myPrefs.setString("jwt", response['token']);
+        setState(() {
+          errorMessage = "";
+        });
+        //Navigator.of(context).pushNamed("/login");
+      }
+      setState(() {
+        loading = false;
+      });
+    } catch (err) {
+      setState(() {
+        errorMessage = err;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    if (loading) {
+      return Loading();
+    }
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
@@ -63,6 +97,7 @@ class _Login extends State<Login> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Container(height: MediaQuery.of(context).padding.top),
+          ErrorMessage(errorMessage: errorMessage),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
             child: TextField(
@@ -75,11 +110,6 @@ class _Login extends State<Login> {
                 ),
                 border: OutlineInputBorder(),
               ),
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
             ),
           ),
           Padding(
@@ -94,30 +124,13 @@ class _Login extends State<Login> {
                 ),
                 border: OutlineInputBorder(),
               ),
-              onChanged: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
             ),
           ),
           Padding(
             padding: EdgeInsets.symmetric(vertical: height * .1),
             child: RaisedButton(
               color: Colors.lightBlueAccent,
-              onPressed: () async {
-                SharedPreferences myPrefs =
-                    await SharedPreferences.getInstance();
-                email = _emailController.text;
-                password = _passwordController.text;
-                var jwt = await attemptLogIn(email, password);
-                if (jwt != null) {
-                  myPrefs.setString("jwt", jwt);
-                  //Navigator.of(context).pushNamed("/login");
-                } else {
-                  return ("No account was found that matches that email and password");
-                }
-              },
+              onPressed: login,
               child: Text(
                 'Login',
                 style: TextStyle(color: Colors.white),
@@ -125,18 +138,15 @@ class _Login extends State<Login> {
             ),
           ),
           Container(
-            child: Center(child: isLoggedIn
-            ? Text("Logged In"):
-            RaisedButton(
-              child: Text("Login with Facebook"),
-              onPressed: ()=> initiateFacebookLogin(),
-              )
-            )
-            )
+              child: Center(
+                  child: isLoggedIn
+                      ? Text("Logged In")
+                      : RaisedButton(
+                          child: Text("Login with Facebook"),
+                          onPressed: () => initiateFacebookLogin(),
+                        )))
         ],
-      )
-      )
-      ),
+      ))),
     );
   }
 }
