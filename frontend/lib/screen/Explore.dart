9ee1e5cffa195e72e8ae424c3abe30025132756f
sd169 <sajith.dhambagolla@gmail.com>
2020-05-23 00:38:02 +1200

    added camera to explore page

--- frontend/lib/screen/Explore.dart
+++ frontend/lib/screen/Explore.dart
@@ -1,61 +1,124 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:camera/camera.dart';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:frontend/widgets/error.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Explore extends StatelessWidget{
+class Explore extends StatefulWidget{
   @override
-  Widget build(BuildContext context){
+  _explore createState() => _explore();
+}
+
+class _explore extends State<Explore>{
+
+  File imageFile;
+  bool loading = false;
+  String errorMessage = "";
+  
 
     void camera() async{
       try{
         var camera = await ImagePicker.pickImage(
-      source: ImageSource.camera,
+          source: ImageSource.camera,
         );
-      }
-      catch(err){
+        this.setState(() {
+          imageFile = camera;
+        });
 
+      if (imageFile == null) return;
+      String base64Image = base64Encode(imageFile.readAsBytesSync());
+      String fileName = imageFile.path.split("/").last;
+      List<String> printOut = new List<String>();
+      var res = await http.post(
+          "https://limitless-meadow-18984.herokuapp.com/user/identify",     
+      body: {
+        "image": base64Image,
+        "name": fileName,
+      });
+        var response = await json.decode(res.body);
+        if (response['error'] != null) {
+        setState(() {
+          errorMessage = response['error']['message'];
+        });
       }
+        print(response['logos']);
+        setState(() {
+          printOut = response['logos']
+              .map((logo) {
+                return logo;
+              })
+              .toList()
+              .cast<String>();
+        });
+      }
+      catch (err) {
+      setState(() {
+        errorMessage = err;
+      });
     }
+    } 
+    
 
     void gallery() async{
       try{
         var gallery = await ImagePicker.pickImage(
           source: ImageSource.gallery,
         );
+        this.setState(() {
+          imageFile = gallery;
+        });
       }
-      catch(err){
-
-      }
+      catch (err) {
+      setState(() {
+        errorMessage = err;
+      });
+    }
     }
 
-    Future<void> _optionsDialogBox(){
+    Future<void> _showChoiceDialog(BuildContext context){
       return showDialog(context: context,
       builder: (BuildContext context){
         return AlertDialog(
-          content: new SingleChildScrollView(
-            child: new ListBody(
+          title: Text("Make a choice"),
+          content: SingleChildScrollView(
+            child: ListBody(
               children: <Widget>[
-                RaisedButton(
-                  child: new Text('Capture your logo'),
-                  onPressed: camera,
-                  ),
-                
+                GestureDetector(
+                  child:  Text('Gallery'),
+                  onTap: (){
+                    gallery();
+                  } 
+                ),
+                GestureDetector(
+                  child: Text('Camera'),
+                  onTap: (){
+                    camera();
+                  },
+                )
               ]
             )
           )
           );
       }
       );
-      
     }
     
-    return MaterialApp(    
-      home: new Container(
+
+    
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+    
+      body: new Container(      
         color: Colors.blueAccent,
         child: new Column(
           children: <Widget>[
           new Row(
             children: <Widget>[
+              ErrorMessage(errorMessage: errorMessage),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: RaisedButton(
@@ -64,18 +127,23 @@ class Explore extends StatelessWidget{
                     style: TextStyle(color: Colors.white),
                     ),
                     color: Colors.lightBlueAccent,
-                    onPressed: camera,           
+                    onPressed: (){
+                    _showChoiceDialog(context);
+                  },            
                 ),
               ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
                 child: RaisedButton(
+                  onPressed: (){
+                    _showChoiceDialog(context);
+                  }, 
                   child: Text(
                     'Product',
                     style: TextStyle(color: Colors.white),
                     ),
                     color: Colors.lightBlueAccent,
-                    onPressed: gallery,           
+                               
                 ),
                 
               )
@@ -83,9 +151,12 @@ class Explore extends StatelessWidget{
           )
         ],)
       )
-    );
+      );
+    }
+    
+  
   }
-}
+
 
 
  
