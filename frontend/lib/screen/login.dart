4248828486529eb2aab870bd4ecc5ba9e207e468
sd169 <sajith.dhambagolla@gmail.com>
2020-05-23 16:29:18 +1200

    accidenty made a change on the development

--- frontend/lib/screen/login.dart
+++ frontend/lib/screen/login.dart
@@ -84,6 +84,7 @@ class _Login extends State<Login> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+    bool _showPassword = false;
     if (loading) {
       return Loading();
     }
