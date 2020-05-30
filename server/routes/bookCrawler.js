cd24c5df283b10e094d1ae6aea9d503a363f6191
uskeenonmedia <officialkcwebx@gmail.com>
2020-05-30 21:13:21 +1200

    Added the ability to crawl sub categories

--- server/routes/bookCrawler.js
+++ server/routes/bookCrawler.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   getCategories,
   getPageLinks,
   getBookData,
-} = require("../helpers/bookCrawler");
+  getExtraCategories,
+} = require('../helpers/bookCrawler');
 
-router.get("/categories", getCategories);
-router.get("/getPageLinks", getPageLinks);
-router.get("/getBookData", getBookData);
+router.get('/categories', getCategories);
+router.get('/getPageLinks', getPageLinks);
+router.get('/getBookData', getBookData);
+router.get('/subCategories', getExtraCategories);
 
 module.exports = router;
