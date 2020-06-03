313bab3a2a181f5b06f8eb508cb8761e79bb90e3
uskeenonmedia <officialkcwebx@gmail.com>
2020-06-03 13:37:01 +1200

    Finished the saved routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const router = express.Router();
-const { login, register, addSavedBook, addSavedLogo } = require("../handlers/user")
+const { login, register, addSavedBook, addSavedLogo, getUserSavedBooks, getUserSavedLogos } = require("../handlers/user")
 const { checkJwt } = require('../middleware/user');
 
 router.post('/login', login);
 
 router.post('/register', register);
 
-router.post('/saved/book', checkJwt, addSavedBook)
+router.post('/saved/book/add', checkJwt, addSavedBook)
 
-router.post('/saved/logo', checkJwt, addSavedLogo)
+router.post('/saved/logo/add', checkJwt, addSavedLogo);
+
+router.get('/saved/books', checkJwt, getUserSavedBooks);
+
+router.get('/saved/logos', checkJwt, getUserSavedLogos)
 
 module.exports = router
