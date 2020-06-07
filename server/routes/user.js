74af94d3bbb40693bda7baff885d8ba47da1652a
uskeenonmedia <officialkcwebx@gmail.com>
2020-06-08 00:11:27 +1200

    Added the delete routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,18 +1,31 @@
-const express = require("express")
+const express = require('express');
 const router = express.Router();
-const { login, register, addSavedBook, addSavedLogo, getUserSavedBooks, getUserSavedLogos } = require("../handlers/user")
+const {
+  login,
+  register,
+  addSavedBook,
+  addSavedLogo,
+  getUserSavedBooks,
+  getUserSavedLogos,
+  deleteBook,
+  deleteLogo,
+} = require('../handlers/user');
 const { checkJwt } = require('../middleware/user');
 
 router.post('/login', login);
 
 router.post('/register', register);
 
-router.post('/saved/book/add', checkJwt, addSavedBook)
+router.post('/saved/book/add', checkJwt, addSavedBook);
 
 router.post('/saved/logo/add', checkJwt, addSavedLogo);
 
 router.get('/saved/books', checkJwt, getUserSavedBooks);
 
-router.get('/saved/logos', checkJwt, getUserSavedLogos)
+router.get('/saved/logos', checkJwt, getUserSavedLogos);
 
-module.exports = router
+router.post('/saved/logo/delete', checkJwt, deleteLogo);
+
+router.post('/saved/book/delete', checkJwt, deleteBook);
+
+module.exports = router;
